adf8720c485abfac8d9e730e5b9fbcb7adfff5b5
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-12 16:07:14 -0300

    Test: Se agrega test de persistencia de cliente

---
+++ spec/persistencia/repositorio_clientes_spec.rb
@@ -0,0 +1,11 @@
+require 'integration_helper'
+require_relative '../../persistencia/repositorio_clientes'
+require_relative '../../modelos/cliente'
+
+describe RepositorioClientes do
+  xit 'deberia guardar y asignar id al cliente' do
+    juan = Cliente.new('Juan', 'Av Las Heras 1232', 'CP: 1425')
+    described_class.new.save(juan)
+    expect(juan.id).not_to be_nil
+  end
+end
