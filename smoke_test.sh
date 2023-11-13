899d638784ce73117ec850b980b3178ffcb60322
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-13 09:30:21 -0300

    Feat: se agregan arreglos de network y verificación de deploy para test y prod

---
+++ smoke_test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TARGET_URL=$1
+VERSION=$2
+
+i="0"
+
+while [ $i -lt 3 ]
+do
+  echo "Smoke test"
+  curl -k $TARGET_URL/version | grep $VERSION
+  if [ "$?" = "0" ];
+  then
+  	echo "Test:OK"
+  	exit 0
+  fi
+  i=$(( $i+1 ))
+  sleep 45
+done
+
+echo "Test:FAIL"
+exit 1
