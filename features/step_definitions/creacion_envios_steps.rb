7ba21286d225ae68f2696a5f05080d016cdc1550
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-14 11:20:38 -0300

    Fix: se saca step repetido

--- features/step_definitions/creacion_envios_steps.rb
+++ features/step_definitions/creacion_envios_steps.rb
@@ -2,10 +2,6 @@ Dado('que estoy registrado como cliente') do
   pending
 end
 
-Cuando('envio el mensaje {string}') do |_string|
-  pending
-end
-
 Entonces('se creo el envio con el ID {int}') do |_id|
   pending
 end
