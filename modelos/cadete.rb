11d4acfdc508e099a9de1fb9c21f63c4ff7f416e
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-13 23:54:45 -0300

    Feat: creacion de cadete valida

--- modelos/cadete.rb
+++ modelos/cadete.rb
@@ -1,3 +1,14 @@
 require 'active_model'
 class Cadete
+  include ActiveModel::Validations
+
+  attr_accessor :id, :nombre, :vehiculo
+
+  validates :nombre, presence: true
+
+  def initialize(nombre, vehiculo, id = nil)
+    @nombre = nombre
+    @vehiculo = vehiculo
+    @id = id
+  end
 end
--- spec/cadete_spec.rb
+++ spec/cadete_spec.rb
@@ -3,7 +3,7 @@ require_relative '../modelos/cadete'
 
 describe 'cadete' do
   describe 'valido?' do
-    xit 'Se crea exitosamente un cadete al ingresar todos los datos correctamente' do
+    it 'Se crea exitosamente un cadete al ingresar todos los datos correctamente' do
       cadete = Cadete.new('Pedro', 'moto')
 
       expect(cadete.valid?).to eq true
