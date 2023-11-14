b052a6812d33ad4a0a8c58a38580e409b381ec29
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-13 23:51:05 -0300

    Test: test unitario de registracion de cadete

---
+++ modelos/cadete.rb
@@ -0,0 +1,3 @@
+require 'active_model'
+class Cadete
+end
---
+++ spec/cadete_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require_relative '../modelos/cadete'
+
+describe 'cadete' do
+  describe 'valido?' do
+    xit 'Se crea exitosamente un cadete al ingresar todos los datos correctamente' do
+      cadete = Cadete.new('Pedro', 'moto')
+
+      expect(cadete.valid?).to eq true
+    end
+  end
+end
