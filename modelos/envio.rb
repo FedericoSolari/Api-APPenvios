5fd43e1f51ade43bc84d6daaa766984a3a73965f
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-15 12:05:28 -0300

    Feat: agregado de id de cadete al envio

--- modelos/envio.rb
+++ modelos/envio.rb
@@ -2,15 +2,16 @@ require 'active_model'
 class Envio
   include ActiveModel::Validations
 
-  attr_accessor :id
+  attr_accessor :id, :id_cadete
   attr_reader :direccion, :codigo_postal, :id_cliente
 
   validates :direccion, presence: true
 
-  def initialize(direccion, codigo_postal, id_cliente, id = nil)
+  def initialize(direccion, codigo_postal, id_cliente, id_cadete = nil, id = nil)
     @direccion = direccion
     @codigo_postal = codigo_postal
     @id_cliente = id_cliente
+    @id_cadete = id_cadete
     @id = id
   end
 end
