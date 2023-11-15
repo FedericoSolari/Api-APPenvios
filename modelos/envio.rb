c97ee2cfc3bc25bfa274ea991151880751a504fe
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-15 18:15:59 -0300

    Feat: agrego estado al envio

--- modelos/envio.rb
+++ modelos/envio.rb
@@ -3,15 +3,17 @@ class Envio
   include ActiveModel::Validations
 
   attr_accessor :id, :id_cadete
-  attr_accessor :direccion, :codigo_postal, :id_cliente
+  attr_accessor :direccion, :codigo_postal, :id_cliente, :estado
 
   validates :direccion, presence: true
-
-  def initialize(direccion, codigo_postal, id_cliente, id_cadete = nil, id = nil)
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(direccion, codigo_postal, id_cliente, id_cadete = nil, id = nil, estado = 'pendiente')
     @direccion = direccion
     @codigo_postal = codigo_postal
     @id_cliente = id_cliente
     @id_cadete = id_cadete
     @id = id
+    @estado = estado
   end
+  # rubocop:enable Metrics/ParameterLists
 end
