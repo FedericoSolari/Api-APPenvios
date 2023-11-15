be7a1963f6fdce04c95bab99e9a0dc97450efd3b
Gabriel-Re <gre@fi.uba.ar>
2023-11-15 09:11:57 -0300

    Refactor: ParseadorSolicitudes retorna el body correspondiente

--- modelos/ayudantes/parseador_solicitudes.rb
+++ modelos/ayudantes/parseador_solicitudes.rb
@@ -4,9 +4,20 @@ class ParseadorSolicitudes
     case comando
     when '/registrar'
       regex = %r{^(/registrar)\s+(\w+),\s+(.*?),\s+(CP:\s+\d+),\s+(\d+)$}
+      generar_cuerpo_solicitud_cliente(mensaje.match(regex))
     when '/registrar_cadete'
       regex = %r{^(/registrar_cadete)\s+(\w+),\s+(\w+),\s+(\d+)$}
+      generar_cuerpo_solicitud_cadete(mensaje.match(regex))
     end
-    mensaje.match(regex)
+  end
+
+  private
+
+  def generar_cuerpo_solicitud_cliente(parametros)
+    { comando: parametros[1], body: { nombre: parametros[2], direccion: parametros[3], codigo_postal: parametros[4], id_cliente: parametros[5] } }
+  end
+
+  def generar_cuerpo_solicitud_cadete(parametros)
+    { comando: parametros[1], body: { nombre: parametros[2], vehiculo: parametros[3], id_cadete: parametros[4] } }
   end
 end
--- spec/parseador_solicitudes_spec.rb
+++ spec/parseador_solicitudes_spec.rb
@@ -1,36 +1,26 @@
 require_relative '../modelos/ayudantes/parseador_solicitudes'
 
 describe ParseadorSolicitudes do
-  it 'Debería retornar 6 parámetros al registrar cliente' do
-    mensaje = '/registrar Juan, Av Las Heras 1232, CP: 1425, 8'
-    parametros_esperados = 6
-    expect(described_class.new.parsear(mensaje).length).to eq parametros_esperados
-  end
-
   it 'Debería retornar los parámetros de registrar cliente' do
     mensaje = '/registrar Juan, Av Las Heras 1232, CP: 1425, 8'
-    parametros_esperados = ['/registrar', 'Juan', 'Av Las Heras 1232', 'CP: 1425', '8']
+    parametros_esperados = { comando: '/registrar', body: { nombre: 'Juan', direccion: 'Av Las Heras 1232',
+                                                            codigo_postal: 'CP: 1425', id_cliente: '8' } }
     parametros_obtenidos = described_class.new.parsear(mensaje)
-    expect(parametros_obtenidos[1]).to eq parametros_esperados[0]
-    expect(parametros_obtenidos[2]).to eq parametros_esperados[1]
-    expect(parametros_obtenidos[3]).to eq parametros_esperados[2]
-    expect(parametros_obtenidos[4]).to eq parametros_esperados[3]
-    expect(parametros_obtenidos[5]).to eq parametros_esperados[4]
-  end
-
-  it 'Debería retornar 5 parámetros al registrar cadete' do
-    mensaje = '/registrar_cadete Juan, Moto, 8'
-    parametros_esperados = 5
-    expect(described_class.new.parsear(mensaje).length).to eq parametros_esperados
+    expect(parametros_obtenidos[:comando]).to eq parametros_esperados[:comando]
+    expect(parametros_obtenidos[:body][:nombre]).to eq parametros_esperados[:body][:nombre]
+    expect(parametros_obtenidos[:body][:direccion]).to eq parametros_esperados[:body][:direccion]
+    expect(parametros_obtenidos[:body][:codigo_postal]).to eq parametros_esperados[:body][:codigo_postal]
+    expect(parametros_obtenidos[:body][:id_cliente]).to eq parametros_esperados[:body][:id_cliente]
   end
 
   it 'Debería retornar los parámetros de registrar cadete' do
     mensaje = '/registrar_cadete Juan, Moto, 8'
-    parametros_esperados = ['/registrar_cadete', 'Juan', 'Moto', '8']
+    parametros_esperados = { comando: '/registrar_cadete', body: { nombre: 'Juan', vehiculo: 'Moto',
+                                                                   id_cadete: '8' } }
     parametros_obtenidos = described_class.new.parsear(mensaje)
-    expect(parametros_obtenidos[1]).to eq parametros_esperados[0]
-    expect(parametros_obtenidos[2]).to eq parametros_esperados[1]
-    expect(parametros_obtenidos[3]).to eq parametros_esperados[2]
-    expect(parametros_obtenidos[4]).to eq parametros_esperados[3]
+    expect(parametros_obtenidos[:comando]).to eq parametros_esperados[:comando]
+    expect(parametros_obtenidos[:body][:nombre]).to eq parametros_esperados[:body][:nombre]
+    expect(parametros_obtenidos[:body][:vehiculo]).to eq parametros_esperados[:body][:vehiculo]
+    expect(parametros_obtenidos[:body][:id_cadete]).to eq parametros_esperados[:body][:id_cadete]
   end
 end
