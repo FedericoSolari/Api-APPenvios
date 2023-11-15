9e86a1a5b9d0e796f3f17f44d96dc97d7e394d3c
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-15 08:36:30 -0300

    Feat: devuelve parametros esperados al parsear mensaje de cliente

--- modelos/ayudantes/parseador_solicitudes.rb
+++ modelos/ayudantes/parseador_solicitudes.rb
@@ -1,5 +1,6 @@
 class ParseadorSolicitudes
-  def parsear(_mensaje)
-    5
+  def parsear(mensaje)
+    regex = %r{^(/\w+)\s+(\w+),\s+(.*?),\s+(CP:\s+\d+),\s+(\d+)$}
+    mensaje.match(regex)
   end
 end
--- spec/parseador_solicitudes_spec.rb
+++ spec/parseador_solicitudes_spec.rb
@@ -1,20 +1,26 @@
 require_relative '../modelos/ayudantes/parseador_solicitudes'
 
 describe ParseadorSolicitudes do
-  it 'Debería retornar 5 parámetros al registrar cliente' do
+  it 'Debería retornar 6 parámetros al registrar cliente' do
     mensaje = '/registrar Juan, Av Las Heras 1232, CP: 1425, 8'
-    parametros_esperados = 5
-    expect(described_class.new.parsear(mensaje)).to eq parametros_esperados
+    parametros_esperados = 6
+    expect(described_class.new.parsear(mensaje).length).to eq parametros_esperados
   end
 
-  xit 'Debería retornar los parámetros de registrar cliente enviados' do
+  it 'Debería retornar los parámetros de registrar cliente enviados' do
     mensaje = '/registrar Juan, Av Las Heras 1232, CP: 1425, 8'
-    parametros_esperados = ['/registrar', 'Juan', 'Av Las Heras 1232', 'CP: 1425', 8]
+    parametros_esperados = ['/registrar', 'Juan', 'Av Las Heras 1232', 'CP: 1425', '8']
     parametros_obtenidos = described_class.new.parsear(mensaje)
     expect(parametros_obtenidos[1]).to eq parametros_esperados[0]
     expect(parametros_obtenidos[2]).to eq parametros_esperados[1]
     expect(parametros_obtenidos[3]).to eq parametros_esperados[2]
     expect(parametros_obtenidos[4]).to eq parametros_esperados[3]
     expect(parametros_obtenidos[5]).to eq parametros_esperados[4]
+  end
+
+  xit 'Debería retornar 5 parámetros al registrar cadete' do
+    mensaje = '/registrar Juan, Moto, 8'
+    parametros_esperados = 5
+    expect(described_class.new.parsear(mensaje).length).to eq parametros_esperados
   end
 end
