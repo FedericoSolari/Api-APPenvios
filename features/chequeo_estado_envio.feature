2000c5abb669f50f8cdac7666dc9f646c9c53304
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-15 20:41:34 -0300

    Feat: agregado de steps de estado de envio

--- features/chequeo_estado_envio.feature
+++ features/chequeo_estado_envio.feature
@@ -1,18 +1,18 @@
 #language: es
 
-@wip
+# @wip
 Característica: Como cliente quiero ver el estado del envio
 Antecedentes:
 Dado Que estoy registrado como cliente
 Y existe el envio con el ID 1
-Y con direccion "Santa fe 1223, CP: 1400"
+Y con direccion "Santa fe 1223", "CP: 1400"
 
 Escenario: Obtener el estado de un envio sin cadete asignado
 Dado que el envio con ID 1 esta sin asignar
-Cuando Mando el comando "/estado-envio 1"
-Entonces deberia ver un mensaje "Tu envio (ID: 8) se encuentra pendiente de asignación"
+Cuando Mando el comando "/estado_envio" "1"
+Entonces deberia el mensaje "Tu envio (ID: 1) se encuentra pendiente de asignación"
 
 Escenario: Obtener el estado de un envio que ya fue asignado a un cadete
 Dado que el envio con ID 1 esta entregado
-Cuando Mando el comando "/estado-envio 1"
-Entonces deberia ver un mensaje "Tu envio (ID: 8) se encuentra en proceso de entrega"
+Cuando Mando el comando "/estado_envio" "1"
+Entonces deberia el mensaje "Tu envio (ID: 1) se encuentra en proceso de entrega"
--- features/step_definitions/chequeo_estado_envio.rb
+++ features/step_definitions/chequeo_estado_envio.rb
@@ -1,28 +1,41 @@
 Dado('Que estoy registrado como cliente') do
-    # Implementar lógica para registrar al cliente si es necesario
-  end
+    @repositorio_clientes = RepositorioClientes.new
+    @repositorio_clientes.delete_all
+    @cliente = Cliente.new("pedro","una_direccion","un_codigo_postal",1)
+    @repositorio_clientes.save(@cliente)
+end
   
-Dado('existe el envío con el ID {int}') do |envio_id|
-    # Implementar lógica para crear el envío con el ID proporcionado
-  end
+Dado('existe el envio con el ID {int}') do |_envio_id|
+    @repositorio_envios = RepositorioEnvios.new
+    @repositorio_envios.delete_all
+    @envio = Envio.new("una_direccion","un_codigo_postal",@cliente.id_cliente)
+end
   
-Dado('con dirección {string}') do |direccion|
-    # Implementar lógica para asignar la dirección al envío
-  end
+  Dado('con direccion {string}, {string}') do |direccion, codigo_postal|
+    @envio.direccion = direccion
+    @envio.codigo_postal = codigo_postal
+    @repositorio_envios.save(@envio)
+end
   
-Dado('que el envío con ID {int} está sin asignar') do |envio_id|
-    # Implementar lógica para marcar el envío como sin asignar
-  end
+Dado('que el envio con ID {int} esta sin asignar') do |_envio_id|
+end
   
-Cuando('mando el comando {string}') do |comando|
-    # Implementar lógica para simular el envío del comando
-  end
+Dado('que el envio con ID {int} esta entregado') do |_envio_id|
+end
   
-Entonces('debería ver un mensaje {string}') do |mensaje|
-    # Implementar lógica para verificar que se muestra el mensaje esperado
-  end
+Cuando('Mando el comando {string} {string}') do |_comando, _id|
+    @response = Faraday.get("/envios/#{@envio.id.to_s}", { 'Content-Type' => 'application/json' })
+end
   
-Dado('que el envío con ID {int} está entregado') do |envio_id|
-    # Implementar lógica para marcar el envío como entregado
-  end
+Entonces('deberia el mensaje {string}') do |_mensaje|
+  parsed_response = JSON.parse(@response.body)
+  expect(parsed_response['text'].include?(@envio.id.to_s)).to eq true 
+  expect(parsed_response['text'].include?("pendiente de asignación")).to eq true 
+end
+
+Dado('que el envio con ID {int} está entregado') do |_envio_id|
+  parsed_response = JSON.parse(@response.body)
+  expect(parsed_response['text'].include?(@envio.id.to_s)).to eq true 
+  expect(parsed_response['text'].include?("proceso de entrega")).to eq true 
+end
   
