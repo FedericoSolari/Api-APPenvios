f2f7d47beeb764b8a74d211678cb7a2095c46a19
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-16 13:54:05 -0300

    Feat: se agrega respuesta en caso de error al obtener estado

--- modelos/ayudantes/parseador_estado.rb
+++ modelos/ayudantes/parseador_estado.rb
@@ -1,10 +1,12 @@
 class ParseadorEstado
-  def obtener_mensaje(id_envio, mensaje)
-    case mensaje
+  def obtener_mensaje(id_envio, estado)
+    case estado
     when 'pendiente de asignacion'
       "Tu envio (ID: #{id_envio}) se encuentra pendiente de asignación"
     when 'en proceso'
       "Tu envio (ID: #{id_envio}) se encuentra en proceso de entrega"
+    else
+      'Hubo un error al obtener el estado'
     end
   end
 end
