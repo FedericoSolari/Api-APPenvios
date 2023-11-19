eae492ae09ad0a12a4dc7ee85580f24efce5ea25
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-19 17:56:01 -0300

    Feat: validacion de domicilio

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg'
 gem 'rake'
 # gem 'rack-parser', :require => 'rack/parser'
 gem 'activemodel', require: 'active_model'
+gem 'faraday'
 gem 'semantic_logger'
 gem 'sequel'
 gem 'sinatra-contrib'
@@ -17,7 +18,6 @@ group :development, :test do
   gem 'byebug'
   gem 'cucumber'
   gem 'debug', '>= 1.0.0'
-  gem 'faraday'
   gem 'rack-test', require: 'rack/test'
   gem 'rspec'
   gem 'rspec-core'
@@ -25,4 +25,5 @@ group :development, :test do
   gem 'rubocop', '1.50.2', require: false
   gem 'rubocop-rspec', '2.22.0', require: false
   gem 'simplecov'
+  gem 'webmock'
 end
--- spec/direccion_spec.rb
+++ spec/direccion_spec.rb
@@ -1,38 +1,38 @@
-# require_relative '../modelos/direccion'
+require_relative '../modelos/direccion'
+require 'web_mock'
+require 'faraday'
 
 def cuando_valido_domicilio(direccion, codigo_postal)
   body = { "items": [{
-    "title": '5 Rue Daunou, 75002 Paris, France',
-    "id": 'here:af:streetsection:bI4Le6cyA.1mlQyLunYpjC:CggIBCCi-9SPARABGgE1KGQ',
-    "resultType": 'houseNumber',
-    "houseNumberType": 'PA',
+    "title": 'General Las Heras, C1018AAO Ciudad de Buenos Aires, Argentina',
+    "id": 'here:af:streetsection:ZfBPNA4Pfddw6eWgKetVdC',
+    "resultType": 'street',
     "address": {
-      "label": '5 Rue Daunou, 75002 Paris, France',
-      "countryCode": 'FRA',
-      "countryName": 'France',
-      "stateCode": 'IDF',
-      "state": 'Île-de-France',
-      "county": 'Paris',
-      "city": 'Paris',
-      "district": '2e Arrondissement',
-      "street": 'Rue Daunou',
-      "postalCode": '75002',
-      "houseNumber": '5'
+      "label": 'General Las Heras, C1018AAO Ciudad de Buenos Aires, Argentina',
+      "countryCode": 'ARG',
+      "countryName": 'Argentina',
+      "stateCode": 'CF',
+      "state": 'Ciudad Autónoma de Buenos Aires',
+      "county": 'Ciudad de Buenos Aires',
+      "city": 'Ciudad de Buenos Aires',
+      "district": 'Recoleta',
+      "street": 'General Las Heras',
+      "postalCode": 'C1018AAO'
     },
     "position": {
-      "lat": 48.86926,
-      "lng": 2.3321
+      "lat": -34.59218,
+      "lng": -58.39141
     }
   }] }
 
-  stub_request(:get, "https://geocode.search.hereapi.com/v1/geocode?q=#{direccion},#{codigo_postal}CABA&apiKey=Uzz6vH2XekHzbN852dERrr8_rCR086eq7jAEpQbmzX4")
+  stub_request(:get, "https://geocode.search.hereapi.com/v1/geocode?q=#{direccion},#{codigo_postal}CABA&apiKey=")
     .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
 end
 
 describe 'Direccion' do
-  xit 'Debería retornar los parámetros de registrar cliente' do
+  it 'Debería retornar los parámetros de registrar cliente' do
     cuando_valido_domicilio('Av Las Heras 1232', '1425')
-    direccion = Direccion.new('Av Las Heras 1232', '1425')
+    direccion = Direccion.new('Av Las Heras 1232', 'CP: 1425')
     expect(direccion.valid?).to eq(true)
   end
 end
