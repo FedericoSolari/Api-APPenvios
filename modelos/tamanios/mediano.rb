5d487c2f3c892653b07b41213febfea128b7c84e
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-21 10:53:12 -0300

    Feat-03: creacion de tamaño mediano exitosa

---
+++ modelos/tamanios/mediano.rb
@@ -0,0 +1,10 @@
+class Mediano
+  TAMANIO = 'mediano'.freeze
+  def initialize
+    @tamanio = TAMANIO
+  end
+
+  def tamanio
+    @tamanio
+  end
+end
--- spec/tamanio_envios_spec.rb
+++ spec/tamanio_envios_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../modelos/tamanios/chico'
+require_relative '../modelos/tamanios/mediano'
 
 describe 'Tamaños' do
   # Por ahora solo se va a testear que devuelva el nombre bien, luego va a tener
@@ -12,7 +13,7 @@ describe 'Tamaños' do
   end
 
   describe 'Mediano' do
-    xit 'Envio mediano creado correctamente' do
+    it 'Envio mediano creado correctamente' do
       tamanio = Mediano.new
       tamanio_esperado = 'mediano'
       expect(tamanio.tamanio).to eq tamanio_esperado
