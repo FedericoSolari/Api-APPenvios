a46b10a32e686fdc299564ef8d8eaa31cf11ad62
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-21 10:50:36 -0300

    Feat-03: creacion de tamaño chico exitosa

---
+++ modelos/tamanios/chico.rb
@@ -0,0 +1,10 @@
+class Chico
+  TAMANIO = 'chico'.freeze
+  def initialize
+    @tamanio = TAMANIO
+  end
+
+  def tamanio
+    @tamanio
+  end
+end
--- spec/tamanio_envios_spec.rb
+++ spec/tamanio_envios_spec.rb
@@ -1,8 +1,10 @@
+require_relative '../modelos/tamanios/chico'
+
 describe 'Tamaños' do
   # Por ahora solo se va a testear que devuelva el nombre bien, luego va a tener
   # mayor funcionalidad
   describe 'Chico' do
-    xit 'Envio chico creado correctamente' do
+    it 'Envio chico creado correctamente' do
       tamanio = Chico.new
       tamanio_esperado = 'chico'
       expect(tamanio.tamanio).to eq tamanio_esperado
