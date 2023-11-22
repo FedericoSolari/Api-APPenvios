74f4551a116b2c3d5e4fc4e1d79e929644ba6228
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-22 01:17:30 -0300

    Refactor-15: parametros recibidos por here routs

--- conectores/conector_here_api_routes.rb
+++ conectores/conector_here_api_routes.rb
@@ -2,13 +2,16 @@ require 'faraday'
 require_relative '../excepciones/conexion_here_api_error'
 
 class ConectorHereApiRoutes
-  def initialize(api_key)
+  def initialize(api_key, tipo_de_retorno = 'summary')
     @api_key = api_key || '-ZgDQiB9BtkuI_pYPTuXDKIEtTX-qjZyPmZJEyVE-Zs'
     @api_url = ENV['HERE_API_ROUTES_URL'] || 'https://router.hereapi.com/v8/routes'
+    @tipo_de_retorno = tipo_de_retorno
   end
 
-  def obtener_duracion_viaje(origen, destino, tipo_de_retorno = 'summary', vehiculo = 'car')
-    respuesta = Faraday.get("#{@api_url}?apiKey=#{@api_key}&return=#{tipo_de_retorno}&transportMode=#{vehiculo}&origin=#{origen}&destination=#{destino}")
+  def obtener_duracion_viaje(latitud_origen, longitud_origen, latitud_destino, longitud_destino, vehiculo = 'car')
+    origen = "#{latitud_origen}%2C#{longitud_origen}"
+    destino = "#{latitud_destino}%2C#{longitud_destino}"
+    respuesta = Faraday.get("#{@api_url}?apiKey=#{@api_key}&return=#{@tipo_de_retorno}&transportMode=#{vehiculo}&origin=#{origen}&destination=#{destino}")
     parseador_respuesta(respuesta)
   rescue Faraday::Error => e
     raise ConexionHereApiError, e.message
--- spec/conector_here_routes_spec.rb
+++ spec/conector_here_routes_spec.rb
@@ -3,10 +3,12 @@ require_relative '../conectores/conector_here_api_routes'
 describe 'ConectorHereApiRoutes' do
   it 'Estado de respuesta de HereApiRoutes OK' do
     WebMock.allow_net_connect!
-    origen = '-34.59218%2C-58.39141'
-    destino = '-34.59218%2C-58.39141'
+    latitud_origen = '-34.59218'
+    longitud_origen = '-58.39141'
+    latitud_destino = '-34.59218'
+    longitud_destino = '-58.39141'
     response = ConectorHereApiRoutes.new('-ZgDQiB9BtkuI_pYPTuXDKIEtTX-qjZyPmZJEyVE-Zs').obtener_duracion_viaje(
-      origen, destino
+      latitud_origen, longitud_origen, latitud_destino, longitud_destino
     )
     expect(response['routes'].empty?).to eq(false)
   end
