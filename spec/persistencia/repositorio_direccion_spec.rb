ace8da65f85512cf79e3139dfae05d7efea12c38
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-21 21:18:02 -0300

    Test-15: test de buscar por direccion y codigo postal

--- spec/persistencia/repositorio_direccion_spec.rb
+++ spec/persistencia/repositorio_direccion_spec.rb
@@ -1,5 +1,6 @@
 require 'integration_helper'
 require_relative '../../modelos/direccion'
+# require_relative '../../persistencia/repositorio_direcciones'
 
 describe 'RepositorioDirecciones' do
   xit 'deberia guardar y asignar id a la direccion' do
@@ -13,5 +14,12 @@ describe 'RepositorioDirecciones' do
     repositorio = RepositorioDirecciones.new
     repositorio.save(direccion)
     expect(repositorio.find_by_id(direccion.id)).not_to be_nil
+  end
+
+  xit 'deberia encontrar el registro segun el id de la direccion' do
+    direccion = Direccion.new('Av Las Heras 1232', 'CP: 1018')
+    repositorio = RepositorioDirecciones.new
+    repositorio.save(direccion)
+    expect(repositorio.find_by_address(direccion.direccion, direccion.codigo_postal)).not_to be_nil
   end
 end
