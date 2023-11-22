847916d657f2b58043faccdd7904b0fafc8e507c
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-22 09:26:53 -0300

    Feat: se agrega validador de parametros de registro de cliente

--- modelos/ayudantes/validador_parametros.rb
+++ modelos/ayudantes/validador_parametros.rb
@@ -2,7 +2,7 @@ require_relative '../../excepciones/parametros_invalidos_error'
 
 class ValidadorParametros
   def validar_registro_cliente(parametros)
-    if parametros['nombre'].nil?
+    if parametros['nombre'].nil? || parametros['direccion'].nil? || parametros['codigo_postal'].nil? || parametros['id_cliente'].nil?
       raise ParametrosInvalidosError, 'Verifique haber ingresado los datos necesarios, el formato correcto es: \<Nombre\>, \<Domicilio\> \<Altura\>, CP: \<codigo postal\>'
     end
 
--- spec/validador_parametros_spec.rb
+++ spec/validador_parametros_spec.rb
@@ -8,5 +8,29 @@ describe ValidadorParametros do
       parametros = JSON.parse(parametros_enviados)
       expect { described_class.new.validar_registro_cliente(parametros) }.to raise_error(ParametrosInvalidosError, 'Verifique haber ingresado los datos necesarios, el formato correcto es: \<Nombre\>, \<Domicilio\> \<Altura\>, CP: \<codigo postal\>')
     end
+
+    it 'Debería lanzar excepción de parametros invalidos cuando falta la direccion' do
+      parametros_enviados = { nombre: 'Juan', codigo_postal: 'CP: 1010', id_cliente: 8 }.to_json
+      parametros = JSON.parse(parametros_enviados)
+      expect { described_class.new.validar_registro_cliente(parametros) }.to raise_error(ParametrosInvalidosError, 'Verifique haber ingresado los datos necesarios, el formato correcto es: \<Nombre\>, \<Domicilio\> \<Altura\>, CP: \<codigo postal\>')
+    end
+
+    it 'Debería lanzar excepción de parametros invalidos cuando falta el código postal' do
+      parametros_enviados = { nombre: 'Juan', direccion: 'Cerrito 628', id_cliente: 8 }.to_json
+      parametros = JSON.parse(parametros_enviados)
+      expect { described_class.new.validar_registro_cliente(parametros) }.to raise_error(ParametrosInvalidosError, 'Verifique haber ingresado los datos necesarios, el formato correcto es: \<Nombre\>, \<Domicilio\> \<Altura\>, CP: \<codigo postal\>')
+    end
+
+    it 'Debería lanzar excepción de parametros invalidos cuando falta el id del cliente' do
+      parametros_enviados = { nombre: 'Juan', direccion: 'Cerrito 628', codigo_postal: 'CP: 1010' }.to_json
+      parametros = JSON.parse(parametros_enviados)
+      expect { described_class.new.validar_registro_cliente(parametros) }.to raise_error(ParametrosInvalidosError, 'Verifique haber ingresado los datos necesarios, el formato correcto es: \<Nombre\>, \<Domicilio\> \<Altura\>, CP: \<codigo postal\>')
+    end
+
+    it 'Debería devolver true cuando estan todos los parametros correctamente' do
+      parametros_enviados = { nombre: 'Juan', direccion: 'Cerrito 628', codigo_postal: 'CP: 1010', id_cliente: 8 }.to_json
+      parametros = JSON.parse(parametros_enviados)
+      expect(described_class.new.validar_registro_cliente(parametros)).to eq true
+    end
   end
 end
