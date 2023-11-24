f6240e15db9e1a3aee909df8df8c44540825d3cc
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-24 17:23:06 -0300

    Test-2.1: validar vehiculo bicicleta

--- spec/validador_parametros_spec.rb
+++ spec/validador_parametros_spec.rb
@@ -99,6 +99,13 @@ describe ValidadorParametros do
 
       expect(described_class.new.validar_vehiculo(parametros['vehiculo'])).to eq true
     end
+
+    xit 'Validador de vehiculos deberia devolver true cuando el vehiculo es Bicicleta' do
+      parametros_enviados = { vehiculo: 'bicicleta' }.to_json
+      parametros = JSON.parse(parametros_enviados)
+
+      expect(described_class.new.validar_vehiculo(parametros['vehiculo'])).to eq true
+    end
   end
 
   describe 'Creación de envio' do
