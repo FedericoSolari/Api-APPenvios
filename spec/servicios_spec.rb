fd2c7beebe2f61d258e4f2dec5c0a5a38ca4d8c7
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-23 23:14:49 -0300

    Test: servicio direccion

--- spec/servicios_spec.rb
+++ spec/servicios_spec.rb
@@ -12,4 +12,15 @@ describe 'Servicios' do
       end
     end
   end
+
+  describe 'ServicioDireccion' do
+    describe 'Direccion valida?' do
+      it 'Se crea exitosamente una direccion al ingresar todos los datos correctamente' do
+        parametros_direccion = { 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018' }
+        direccion = ServicioDireccion.obtener_direccion(parametros_direccion['direccion'], parametros_direccion['codigo_postal'])
+
+        expect(direccion.valid?).to eq true
+      end
+    end
+  end
 end
