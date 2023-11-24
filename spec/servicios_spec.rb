237ec835bfe9154118ff9ddcf8eda43f818f33e5
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-23 23:25:57 -0300

    Test: servio envio

--- spec/servicios_spec.rb
+++ spec/servicios_spec.rb
@@ -1,26 +1,36 @@
 require 'spec_helper'
 require_relative '../servicios/servicio_usuarios'
+require_relative '../servicios/servicio_direccion'
+require_relative '../servicios/servicio_envio'
 
 describe 'Servicios' do
   describe 'ServicioUsuarios' do
-    describe 'cliente valido?' do
-      it 'Se crea exitosamente un cliente al ingresar todos los datos correctamente' do
-        parametros_cliente = { 'nombre' => 'juan', 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018', 'id_cliente' => 8 }
-        cliente = ServicioUsuarios.agregar_cliente(parametros_cliente)
+    it 'Se crea exitosamente un cliente al ingresar todos los datos correctamente' do
+      parametros_cliente = { 'nombre' => 'juan', 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018', 'id_cliente' => 8 }
+      cliente = ServicioUsuarios.agregar_cliente(parametros_cliente)
 
-        expect(cliente.valid?).to eq true
-      end
+      expect(cliente.valid?).to eq true
     end
   end
 
   describe 'ServicioDireccion' do
-    describe 'Direccion valida?' do
-      it 'Se crea exitosamente una direccion al ingresar todos los datos correctamente' do
-        parametros_direccion = { 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018' }
-        direccion = ServicioDireccion.obtener_direccion(parametros_direccion['direccion'], parametros_direccion['codigo_postal'])
+    it 'Se crea exitosamente una direccion al ingresar todos los datos correctamente' do
+      parametros_direccion = { 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018' }
+      direccion = ServicioDireccion.obtener_direccion(parametros_direccion['direccion'], parametros_direccion['codigo_postal'])
 
-        expect(direccion.valid?).to eq true
-      end
+      expect(direccion.valid?).to eq true
+    end
+  end
+
+  describe 'ServicioEnvio' do
+    it 'Se crea exitosamente un envio al ingresar todos los datos correctamente' do
+      parametros_cliente = { 'nombre' => 'juan', 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018', 'id_cliente' => 8 }
+      cliente = ServicioUsuarios.agregar_cliente(parametros_cliente)
+      RepositorioClientes.new.save(cliente)
+      parametros_envio = { 'tamanio' => 'chico', 'direccion' => 'Av Las Heras 1232', 'codigo_postal' => 'CP: 1018', 'id_cliente' => 8 }
+      envio = ServicioEnvio.agregar_envio(parametros_envio)
+
+      expect(envio.valid?).to eq true
     end
   end
 end
