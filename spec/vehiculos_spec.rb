d70d64c330a760710791b4ef3524206717de24ec
Gabriel-Re <gre@fi.uba.ar>
2023-11-28 20:09:29 -0300

    Test-05: Se crea creacion de moto con fabrica

--- spec/vehiculos_spec.rb
+++ spec/vehiculos_spec.rb
@@ -36,5 +36,13 @@ describe 'Vehiculos' do
 
       expect(vehiculo_creado.vehiculo).to eq vehiculo_a_crear
     end
+
+    xit 'Fabrica crea correctamente un vehiculo moto' do
+      fabrica = FabricaVehiculos.new
+      vehiculo_a_crear = 'moto'
+      vehiculo_creado = fabrica.crear_vehiculo(vehiculo_a_crear)
+
+      expect(vehiculo_creado.vehiculo).to eq vehiculo_a_crear
+    end
   end
 end
