a70dd16d624595837a576e854fa72fa7c3bb0659
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-28 14:46:29 -0300

    Test: estado asignado no puede cambiar a estado pendiente

--- spec/estados_spec.rb
+++ spec/estados_spec.rb
@@ -78,4 +78,16 @@ describe 'Estados' do
       )
     end
   end
+
+  describe 'Pendiente de asignación' do
+    xit 'Estado asignado no puede cambiar a estado pendiente de asignación' do
+      estado = Pendiente.new
+
+      expect do
+        estado.cambiar_asignado
+      end.to raise_error(
+               CambioEstadoInvalidoError, 'El envio ya se encuentra asignado.'
+             )
+    end
+  end
 end
