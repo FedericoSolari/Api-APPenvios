6c5c3952a4bf063f5c852640c397728cd5e5b3f3
Gabriel-Re <gre@fi.uba.ar>
2023-11-28 20:12:24 -0300

    Test-05: Se crea test para creacion de bicicleta con fabrica

--- spec/vehiculos_spec.rb
+++ spec/vehiculos_spec.rb
@@ -44,5 +44,13 @@ describe 'Vehiculos' do
 
       expect(vehiculo_creado.vehiculo).to eq vehiculo_a_crear
     end
+
+    xit 'Fabrica crea correctamente un vehiculo bicicleta' do
+      fabrica = FabricaVehiculos.new
+      vehiculo_a_crear = 'bicicleta'
+      vehiculo_creado = fabrica.crear_vehiculo(vehiculo_a_crear)
+
+      expect(vehiculo_creado.vehiculo).to eq vehiculo_a_crear
+    end
   end
 end
