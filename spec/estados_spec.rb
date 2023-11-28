0fe7f3f47775f82a04e2c149975da1fd2759dd78
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-11-28 15:07:59 -0300

    Test: estado en camino no puede cambiar a estado en camino

--- spec/estados_spec.rb
+++ spec/estados_spec.rb
@@ -137,5 +137,15 @@ describe 'Estados' do
         CambioEstadoInvalidoError, 'El envio ya se encuentra en camino a la dirección de entrega.'
       )
     end
+
+    xit 'Estado en camino no puede cambiar a estado en camino' do
+      estado = EnCamino.new
+
+      expect do
+        estado.cambiar_en_camino
+      end.to raise_error(
+        CambioEstadoInvalidoError, 'El envio ya se encuentra en camino a la dirección de entrega.'
+      )
+    end
   end
 end
