475ff59c92c6dcdfc814e5674ef4afd5d09cb3f6
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-29 02:06:26 -0300

    Fix: tipo de envio Clasico por defecto

--- fabricas/fabrica_de_tipos.rb
+++ fabricas/fabrica_de_tipos.rb
@@ -4,10 +4,10 @@ require_relative '../modelos/tipo_de_envio/express'
 class FabricaTipoEnvios
   def crear_tipo_de_envio(tipo)
     case tipo.downcase
-    when 'clasico'
-      Clasico.new
     when 'express'
       Express.new
+    else
+      Clasico.new
     end
   end
 end
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 class Version
   def self.current
-    '10.0.1'
+    '10.0.2'
   end
 end
